7ab8a873f50a90ee73e7823394718ec8ef5185d4
aaronchenghs <aaronchenghs@gmail.com>
2022-11-26 17:36:46 -0600

    Completed monsters rolodex project 1.0 using functional components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import Cardlist from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 
 const App = () =>
 {
+  const [searchField, setSearchField] = useState(''); //[value,setvalue] //'' intializes searchField fcooL!
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  useEffect(() => {
+    console.log('effect fires');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => setMonsters(users));
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
+
+
+  const onSearchChange = (event) => 
+    {
+      const searchFieldValue = event.target.value.toLocaleLowerCase();
+      setSearchField(searchFieldValue);
+    }
+
+  const filterMonsters = monsters.filter((monster) => {
+  return monster.name.toLocaleLowerCase().includes(searchField);
+  })
 
   return(    <div className="App">
   <h1 className='app-title'>Monsters Rolodex!</h1>
-  {/* <SearchBox 
+   <SearchBox 
     className = 'monsters-search-box'
     onChangeHandler = {onSearchChange}
     placeholder = 'search monsters'/>
   
-  <Cardlist monsters = {filteredMonsters}/> */}
+  {<Cardlist monsters = {filterMonsters}/> }
 </div>
 );
 }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,23 +1,14 @@
-import { Component } from 'react'
 import './card-list.styles.css';
 import Card from '../card/card.component';
 
-//this.props accesses props! cool thanks react
-class Cardlist extends Component
-{
+const Cardlist = ({monsters}) => 
+(    
+    <div className = {`card-list`}>
+    {monsters.map(monster => {
+        return <Card monster={monster} />;
+    })}
+    </div>
+);
 
-    render()
-    {
-        const { monsters } = this.props;
-
-        return (
-        <div className = {`card-list ${this.props.monsters}`}>
-            {monsters.map(monster => {
-                return <Card monster={monster} />;
-                })}
-        </div>
-        )
-    }
-}
 
 export default Cardlist;
